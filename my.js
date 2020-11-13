172d6031eb7caabf0de3c492702e67fd37166d2a
maha <maha.suguru@gmail.com>
2020-11-13 13:34:44 +0530

    commiting js and ts file

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
-console.log("am ts filje");
 let name1 = "maha";
 let val = "mahalakshmi";
 function submit() {
     console.log("am inside function");
+    console.log(val);
 }
--- my.ts
+++ my.ts
@@ -1,6 +1,7 @@
-console.log("am ts filje");
+
 let name1 = "maha";
 let val : string= "mahalakshmi";
 function submit(){
     console.log("am inside function");
+    console.log(val);
 }
