21d57b05d6f3d496f705cd5068212cdcbf4fa668
maha <maha.suguru@gmail.com>
2020-11-13 13:22:36 +0530

    commiting js and ts file

---
+++ my.ts
@@ -0,0 +1,6 @@
+console.log("am ts filje");
+let name1 = "maha";
+let val : string= "mahalakshmi";
+function submit(){
+    console.log("am inside function");
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+
+  let employees = [
+    { name: "Employee One", age: 30, joinedYear: 1990, department: "Computer" },
+    { name: "Employee Two", age: 40, joinedYear: 2000, department: "Physics" },
+    { name: "Employee Three", age: 50, joinedYear: 2010, department: "Computer" },
+    {
+      name: "Employee Five",
+      age: 35,
+      joinedYear: 1998,
+      department: "Chemistry",
+    },
+    { name: "Employee Four", age: 20, joinedYear: 2017, department: "Physics" }
+]; 
+console.log("hai am js ");
