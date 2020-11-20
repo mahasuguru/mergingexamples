03ef85f6642fcc38d33fab8094379438096d956c
maha <maha.suguru@gmail.com>
2020-11-20 22:48:25 +0530

    tuples

--- my.js
+++ my.js
@@ -1,8 +1,17 @@
-let name1 = "maha";
-let val = "mahalakshmi";
+var name1 = "maha";
+var val = "mahalakshmi";
 console.log(val);
 function submit() {
     console.log("am inside function");
     console.log(val);
+}
+var person = {
+    name: "John Smith",
+    age: 25,
+    hobbies: ["Dance", "Code"]
+};
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var h = _a[_i];
+    console.log(h.toUpperCase());
 }
 submit();
--- my.ts
+++ my.ts
@@ -1,9 +1,19 @@
 
 let name1 = "maha";
+
 let val : string= "mahalakshmi";
 console.log(val);
 function submit(){
     console.log("am inside function");
     console.log(val);
 }
+let person = {
+    name: "John Smith",
+    age: 25,
+    hobbies: ["Dance", "Code"],
+    };
+    for (const h of person.hobbies) {
+        console.log(h.toUpperCase());
+        }    
+    
 submit();
