7e6ef823d7809ef7b01f541240e62b430243cea6
maha <maha.suguru@gmail.com>
2020-11-20 23:38:47 +0530

    rest operator

--- my.js
+++ my.js
@@ -1,34 +1,8 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var hobbies = ["Swimming", "Music"];
-var activeHobbies = ["Dancing"];
-activeHobbies.push("Reading", "Cycling"); // We can do like this
-activeHobbies.push.apply(// We can do like this
-activeHobbies, hobbies);
-console.log.apply(console, hobbies);
-console.log.apply(console, activeHobbies);
-var person1 = {
-    name: "JOHN",
-    age: 24
-};
-var p1 = person1;
-p1.age = 30;
-console.log(person1);
-console.log(p1); // Both will be updated. As they are referenced with memory.
-var person = {
-    name: "JOHN",
-    age: 24
-};
-var p2 = __assign({}, person);
-p2.age = 30;
-console.log(person);
-console.log(p2);
+function addNumbers() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (prev, curr) { return prev + curr; }, 0);
+}
+console.log(addNumbers(23, 24, 25));
--- my.ts
+++ my.ts
@@ -1,22 +1,4 @@
-let hobbies = ["Swimming", "Music"];
-let activeHobbies = ["Dancing"];
-activeHobbies.push("Reading", "Cycling"); // We can do like this
-activeHobbies.push(...hobbies);
-console.log(...hobbies);
-console.log(...activeHobbies);
-let person1 = {
-name: "JOHN",
-age: 24,
-};
-let p1 = person1;
-p1.age = 30;
-console.log(person1);
-console.log(p1); // Both will be updated. As they are referenced with memory.
-let person = {
-name: "JOHN",
-age: 24,
-};
-let p2 = { ...person };
-p2.age = 30;
-console.log(person);
-console.log(p2);
+function addNumbers(...numbers: number[]) {
+    return numbers.reduce((prev, curr) => prev + curr, 0);
+    }
+    console.log(addNumbers(23, 24, 25));
