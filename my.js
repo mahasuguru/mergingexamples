bb1998b38cefe7f3e9f01ba903a931ea29450fd2
maha <maha.suguru@gmail.com>
2020-11-20 23:04:28 +0530

    union type

--- my.js
+++ my.js
@@ -1,66 +1,20 @@
 var name1 = "maha";
-var val = "mahalakshmi";
-console.log(val);
-function submit() {
-    console.log("am inside function");
-    console.log(val);
+var bar;
+bar = 'maha';
+bar = 45;
+// let foo = string | null ;
+// foo = 'lakshmi' ;
+// foo = null ;
+// foo = undefined // Wont work Once you Turn on strictNullCheck
+function combine(input1, input2) {
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        return input1 + input2;
+    }
+    else {
+        return input1.toString() + input2.toString();
+    }
 }
-var person1 = {
-    name: "John Smith",
-    age: 25,
-    hobbies: ["Dance", "Code"]
-};
-for (var _i = 0, _a = person1.hobbies; _i < _a.length; _i++) {
-    var h = _a[_i];
-    console.log(h.toUpperCase());
-}
-// person.role.push("Employee");
-// person.role[1] = 10; // JS will still run it.
-console.log(person1);
-//Assign Tuple
-var person2 = {
-    name: "John Smith",
-    age: 25,
-    hobbies: ["Dance", "Code"],
-    role: [1, "Admin"]
-};
-person2.role.push("Employee");
-// person.role[1] = 10; // this will throw an error.
-console.log(person2);
-person2.role = [1, "Admin"]; // This is how you add.
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "John Smith",
-    age: 25,
-    hobbies: ["Dance", "Code"],
-    role: Role.ADMIN
-};
-console.log(person);
-//Another Query
-var UserRole;
-(function (UserRole) {
-    UserRole[UserRole["Admin"] = 10] = "Admin";
-    UserRole[UserRole["Basic_User"] = 15] = "Basic_User";
-    UserRole[UserRole["Registered_User"] = 18] = "Registered_User";
-})(UserRole || (UserRole = {}));
-function getRole() {
-    return UserRole.Registered_User;
-}
-var bar = getRole();
-if (bar === UserRole.Registered_User) {
-    console.log("Inside User Role Using Method Call");
-}
-var foo = UserRole.Basic_User;
-if (foo === UserRole.Basic_User) {
-    console.log("Inside User ROle Function Without Using Method Call");
-}
-//You can also get this
-console.log(UserRole.Basic_User);
-console.log(UserRole["Registered_User"]);
-//Show the students what happens when you change the enum to a constant.
-submit();
+var sum = combine(23, 25);
+console.log("SUM is ", sum);
+var fullName = combine("maha ", "lakshmi");
+console.log(fullName);
--- my.ts
+++ my.ts
@@ -1,71 +1,22 @@
 
 let name1 = "maha";
 
-let val : string= "mahalakshmi";
-console.log(val);
-function submit(){
-    console.log("am inside function");
-    console.log(val);
-}
-let person1 = {
-    name: "John Smith",
-    age: 25,
-    hobbies: ["Dance", "Code"],
-    };
-    for (const h of person1.hobbies) {
-        console.log(h.toUpperCase());
-        }    
-       // person.role.push("Employee");
-       // person.role[1] = 10; // JS will still run it.
-        console.log(person1);
-        //Assign Tuple
-        let person2: {
-        name: string;
-        age: number;
-        hobbies: string[];
-        role: [number, string];
-        } = {
-        name: "John Smith",
-        age: 25,
-        hobbies: ["Dance", "Code"],
-        role: [1, "Admin"],
-        };
-        person2.role.push("Employee");
-       // person.role[1] = 10; // this will throw an error.
-        console.log(person2);
-        person2.role = [1, "Admin"]; // This is how you add.
-        enum Role {
-            ADMIN,
-            READ_ONLY,
-            AUTHOR,
+            let bar : number | string;
+            bar = 'maha';
+            bar = 45 ;
+           // let foo = string | null ;
+           // foo = 'lakshmi' ;
+           // foo = null ;
+           // foo = undefined // Wont work Once you Turn on strictNullCheck
+            function combine(input1: string | number, input2: string | number) {
+            if (typeof input1 === "number" && typeof input2 === "number") {
+            return input1 + input2;
+            } else {
+            return input1.toString() + input2.toString();
             }
-            let person = {
-            name: "John Smith",
-            age: 25,
-            hobbies: ["Dance", "Code"],
-            role: Role.ADMIN,
-            };
-            console.log(person);
-            //Another Query
-            enum UserRole {
-            Admin = 10,
-            Basic_User = 15,
-            Registered_User = 18,
             }
-            function getRole() {
-            return UserRole.Registered_User;
-            }
-            let bar = getRole();
-            if (bar === UserRole.Registered_User) {
-            console.log("Inside User Role Using Method Call");
-            }
-            let foo = UserRole.Basic_User;
-            if (foo === UserRole.Basic_User) {
-            console.log("Inside User ROle Function Without Using Method Call");
-            }
-            //You can also get this
-            console.log(UserRole.Basic_User);
-            console.log(UserRole["Registered_User"]);
-            //Show the students what happens when you change the enum to a constant.
-            
-submit();
+            const sum = combine(23, 25);
+            console.log("SUM is ", sum);
+            const fullName = combine("maha ", "lakshmi");
+            console.log(fullName);
+               
