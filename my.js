9724395d5da6a27da566d7ec3b3c4a856e063b33
maha <maha.suguru@gmail.com>
2020-11-20 22:56:46 +0530

    enums

--- my.js
+++ my.js
@@ -5,13 +5,62 @@ function submit() {
     console.log("am inside function");
     console.log(val);
 }
-var person = {
+var person1 = {
     name: "John Smith",
     age: 25,
     hobbies: ["Dance", "Code"]
 };
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+for (var _i = 0, _a = person1.hobbies; _i < _a.length; _i++) {
     var h = _a[_i];
     console.log(h.toUpperCase());
 }
+// person.role.push("Employee");
+// person.role[1] = 10; // JS will still run it.
+console.log(person1);
+//Assign Tuple
+var person2 = {
+    name: "John Smith",
+    age: 25,
+    hobbies: ["Dance", "Code"],
+    role: [1, "Admin"]
+};
+person2.role.push("Employee");
+// person.role[1] = 10; // this will throw an error.
+console.log(person2);
+person2.role = [1, "Admin"]; // This is how you add.
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: "John Smith",
+    age: 25,
+    hobbies: ["Dance", "Code"],
+    role: Role.ADMIN
+};
+console.log(person);
+//Another Query
+var UserRole;
+(function (UserRole) {
+    UserRole[UserRole["Admin"] = 10] = "Admin";
+    UserRole[UserRole["Basic_User"] = 15] = "Basic_User";
+    UserRole[UserRole["Registered_User"] = 18] = "Registered_User";
+})(UserRole || (UserRole = {}));
+function getRole() {
+    return UserRole.Registered_User;
+}
+var bar = getRole();
+if (bar === UserRole.Registered_User) {
+    console.log("Inside User Role Using Method Call");
+}
+var foo = UserRole.Basic_User;
+if (foo === UserRole.Basic_User) {
+    console.log("Inside User ROle Function Without Using Method Call");
+}
+//You can also get this
+console.log(UserRole.Basic_User);
+console.log(UserRole["Registered_User"]);
+//Show the students what happens when you change the enum to a constant.
 submit();
--- my.ts
+++ my.ts
@@ -7,13 +7,65 @@ function submit(){
     console.log("am inside function");
     console.log(val);
 }
-let person = {
+let person1 = {
     name: "John Smith",
     age: 25,
     hobbies: ["Dance", "Code"],
     };
-    for (const h of person.hobbies) {
+    for (const h of person1.hobbies) {
         console.log(h.toUpperCase());
         }    
-    
+       // person.role.push("Employee");
+       // person.role[1] = 10; // JS will still run it.
+        console.log(person1);
+        //Assign Tuple
+        let person2: {
+        name: string;
+        age: number;
+        hobbies: string[];
+        role: [number, string];
+        } = {
+        name: "John Smith",
+        age: 25,
+        hobbies: ["Dance", "Code"],
+        role: [1, "Admin"],
+        };
+        person2.role.push("Employee");
+       // person.role[1] = 10; // this will throw an error.
+        console.log(person2);
+        person2.role = [1, "Admin"]; // This is how you add.
+        enum Role {
+            ADMIN,
+            READ_ONLY,
+            AUTHOR,
+            }
+            let person = {
+            name: "John Smith",
+            age: 25,
+            hobbies: ["Dance", "Code"],
+            role: Role.ADMIN,
+            };
+            console.log(person);
+            //Another Query
+            enum UserRole {
+            Admin = 10,
+            Basic_User = 15,
+            Registered_User = 18,
+            }
+            function getRole() {
+            return UserRole.Registered_User;
+            }
+            let bar = getRole();
+            if (bar === UserRole.Registered_User) {
+            console.log("Inside User Role Using Method Call");
+            }
+            let foo = UserRole.Basic_User;
+            if (foo === UserRole.Basic_User) {
+            console.log("Inside User ROle Function Without Using Method Call");
+            }
+            //You can also get this
+            console.log(UserRole.Basic_User);
+            console.log(UserRole["Registered_User"]);
+            //Show the students what happens when you change the enum to a constant.
+            
 submit();
